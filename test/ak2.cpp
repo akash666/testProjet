6524a8b37477662b6830ed42af880b7458dd6f43
akash666 <akashsinghnitan001@gmail.com>
2020-10-03 09:57:05 +0530

    commit2

---
+++ test/ak2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float k, a;
+	
+	k=2/9;
+	cout<<k<<endl;
+	k=2.0/9;
+	cout<<k<<endl;
+	k=2/9.0;
+	cout<<k<<endl;
+		k=2.0/9.0;
+	cout<<k<<endl;
+	k=9/2;
+	cout<<k<<endl;
+	k=9.0/2;
+	cout<<k<<endl;
+		k=9/2.0;
+	cout<<k<<endl;   
+	
+	
+	k=9.0/2.0;
+	cout<<k<<endl;
+	
+	a=2/9;
+	cout<<a<<endl;
+	a=2.0/9;
+	cout<<a<<endl;
+	a=2/9.0;
+	cout<<a<<endl;
+	a=2.0/9.0;
+	cout<<a<<endl;
+	a=9/2;
+	cout<<a<<endl;
+	a=9.0/2;
+	cout<<a<<endl;
+	a=9/2.0;
+	cout<<a<<endl;
+	a=9.0/2.0;
+	cout<<a<<endl;
+
+	return 0;
+}
